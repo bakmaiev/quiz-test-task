ab6b072719e044cb3b1e3200dc90e6835d550092
bakmaiev <bakmaiev@gmail.com>
2024-02-22 15:31:13 +0200

    Add new changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/quiz-test-task/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
