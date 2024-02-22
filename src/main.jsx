1925ce1e9e1abaf65afd8b1943ef9aa6779d6188
bakmaiev <bakmaiev@gmail.com>
2024-02-22 15:04:49 +0200

    Change basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/quiz-test-task">
       <App />
     </BrowserRouter>
   </React.StrictMode>
